4996ddfeae104ebb1570343fe407771a1647d1ad
YounByoungil <jimmy7525@naver.com>
2022-05-03 17:57:14 +0900

    [feat] Style 알아보기 01

--- react_native_01/App.js
+++ react_native_01/App.js
@@ -7,16 +7,35 @@
  */
 
 import React, {Component} from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 
 class App extends Component {
   render() {
     return (
-      <View>
-        <Text>hello world</Text>
+      <View style={styles.mainView}>
+         <View>
+          <Text>hello world</Text>
+        </View>
+        <View>
+          <Text>hello world</Text>
+        </View>
+        <View>
+          <Text>hello world</Text>
+        </View>
       </View>
+     
     )
   }
 }
+
+const styles = StyleSheet.create ({
+  mainView : {
+    flex : 1, // 화면을 채우는 component 들 간의 차지하는 영역의 비율같은걸 나타내는 지표 
+    backgroundColor : 'green',
+    paddingTop : 50,
+    alignItems : 'center', // 수평정렬 
+    justifyContent : 'center' // 수직정렬
+  }
+})
 
 export default App;
