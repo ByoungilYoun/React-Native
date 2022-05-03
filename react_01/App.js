ab1196fe2479ed3624f2583c2a0fe06fc54d308f
YounByoungil <jimmy7525@naver.com>
2022-05-03 13:39:09 +0900

    [feat] setState 기능 알아보기2 - prevState

--- react_01/App.js
+++ react_01/App.js
@@ -14,7 +14,8 @@ class App extends Component {
 
   state = { // state 는 render 함수 위에 작성
     sampleText : 'Hello World',
-    sampleBoolean : false
+    sampleBoolean : false,
+    sampleNum : 1
   }
 
   inputText = () => ( //inputText 함수 작성 
@@ -38,11 +39,25 @@ class App extends Component {
     } 
   }
 
+  // onAdd = () => {
+  //   this.setState({
+  //     // sampleNum : sampleNum + 1  // 이렇게 하면 에러남. setState 를 통한 데이터 변경을 위해서는 현재 버전을 카피 하고 업데이트를 진행, 이걸 하기 위해서 prevState 라는 현재 버전의 state 값을 setState 의 인자로 넣으면 된다.
+  //   })
+  // }
+
+  onAdd = () => {
+    this.setState(prevState => {
+      return {
+        sampleNum : prevState.sampleNum + 1
+      }
+    })
+  }
+
   render() {
     return (
       <View style={styles.background}>
-        <Text onPress={this.changeState}>
-          {this.state.sampleText}
+        <Text onPress={this.onAdd}>
+          {this.state.sampleNum}
         </Text>
       </View>
     )
