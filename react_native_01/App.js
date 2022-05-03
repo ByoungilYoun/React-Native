e47539ce81952da7077929a8213668ef2136d12a
YounByoungil <jimmy7525@naver.com>
2022-05-03 18:59:16 +0900

    [feat] touch event 알아보기 01

--- react_native_01/App.js
+++ react_native_01/App.js
@@ -8,22 +8,20 @@
 
 import React, {Component} from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import Header from './src/header';
 
 class App extends Component {
+
+
+  state = {
+    appName : 'My First App'
+  }
+
   render() {
     return (
       <View style={styles.mainView}>
-         <View style={styles.subView}>
-          <Text style={styles.mainText}>hello world</Text>
-        </View>
-        <View style={styles.subView}>
-          <Text>hello world</Text>
-        </View>
-        <View style={styles.anotherSubView}>
-          <Text style={styles.mainText}>hello world</Text>
-        </View>
+         <Header name={this.state.appName}/>
       </View>
-     
     )
   }
 }
@@ -31,16 +29,14 @@ class App extends Component {
 const styles = StyleSheet.create ({
   mainView : {
     flex : 1, // 화면을 채우는 component 들 간의 차지하는 영역의 비율같은걸 나타내는 지표 
-    backgroundColor : 'green',
+    backgroundColor : 'white',
     paddingTop : 50,
     alignItems : 'center', // 수평정렬 
     justifyContent : 'center' // 수직정렬
   }, 
   subView : {
-    flex : 1,
     backgroundColor : 'yellow',
     marginBottom : 10,
-    width : '50%'
   },
   anotherSubView : {
     flex : 2, //  총 flex는 4이고 1번째 와 2번째는 flex 가 1이니까 4분의 1씩 크기를 차지하고 마지막 3번째는 flex 가 2니까 4분의 2씩 크기를 차지한다.
@@ -51,8 +47,8 @@ const styles = StyleSheet.create ({
     justifyContent : 'center'
   },
   mainText : {
-    fontSize : 50,
-    fontWeight : 'bold',
+    fontSize : 20,
+    fontWeight : 'normal',
     color : 'red',
     padding : 20
   }
