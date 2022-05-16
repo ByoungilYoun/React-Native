abd5b983b96efe762dc0b50c975373ad178ce8be
YounByoungil <jimmy7525@naver.com>
2022-05-16 16:29:45 +0900

    navigation 실패

--- react_navigation/src/user.js
+++ react_navigation/src/user.js
@@ -1,31 +1,19 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
 import React, { Component } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 // import { createStackNavigator } from '@react-navigation/stack';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-
 import { StyleSheet, View, Text } from 'react-native';
-import HomeScreen from './src/home';
-import UserScreen from './src/user';
+import { enableScreens } from 'react-native-screens';
 
 //const Stack = createStackNavigator(); //screen 이랑 navigator라는 두 개의 property 를 리턴하는 함수 
 const Stack = createNativeStackNavigator();
 
-class App extends Component {
+class UserScreen extends Component {
   render() {
     return(
-      <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen name="Home" component={HomeScreen}/>
-        </Stack.Navigator>
-      </NavigationContainer>
+        <View>
+            <Text>User Screen</Text>
+        </View>
     )
   }
 }
@@ -34,4 +22,4 @@ const styles = StyleSheet.create({
 
 });
 
-export default App;
+export default UserScreen;
