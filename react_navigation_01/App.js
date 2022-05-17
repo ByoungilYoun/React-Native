af0c67fad1f6416f5dd07466273c3873bd3ca965
YounByoungil <jimmy7525@naver.com>
2022-05-17 14:32:41 +0900

    [feat] Header Bar 커스터마이징

--- react_navigation_01/App.js
+++ react_navigation_01/App.js
@@ -7,19 +7,30 @@
  */
 
  import * as React from 'react';
- import { View, Text } from 'react-native';
+ import { View, Text, StyleSheet, Image } from 'react-native';
  import { NavigationContainer } from '@react-navigation/native';
  import { createNativeStackNavigator } from '@react-navigation/native-stack';
  import HomeScreen from './src/home';
  import UserScreen from './src/user';
+ import LogoTitle from './src/logo';
  
  const Stack = createNativeStackNavigator();
  
  function App() {
+
+  logoTitle = () => {
+    return  (
+      <Image
+        style={{width : 40, height : 40}}
+        source={require('./src/assets/pics/home_icon.png')}
+      />
+    )
+  }
+
    return (
      <NavigationContainer>
        <Stack.Navigator
-           screenOptions={{
+           screenOptions={{ // Stack.Navigator 안에 screenOptions 으로 스타일을 주면 모든 속성에 공통적으로 줄 수가 있다. 
            headerStyle : {
                backgroundColor : '#a4511e'
            }, 
@@ -33,7 +44,10 @@
        <Stack.Screen 
           name="Home" 
           component={HomeScreen} 
-          options={{title : 'Home Screen'}}
+          options={{
+            title : 'Home Screen',
+            headerTitle : <LogoTitle/>
+          }}
         />
 
        <Stack.Screen 
