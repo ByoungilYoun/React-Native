2e212c0948d1e2f438370a3fbc0e01d68dd2fdd1
YounByoungil <jimmy7525@naver.com>
2022-05-26 17:11:57 +0900

    [feat] XY Animation 의 추가 효과 및 Triggering Button 생성

--- react_animation/src/Animation01.js
+++ react_animation/src/Animation01.js
@@ -1,24 +1,32 @@
 
 
  import React, { Component } from 'react';
- import { Animated, StyleSheet, View, Text} from 'react-native';
+ import { Easing, Animated, StyleSheet, View, Text, Button } from 'react-native';
  
  class AnimaitonOne extends Component {
    constructor() {
        super();
-       this.mySquare = new Animated.ValueXY(0,0);
+       this.state = {
+        mySquare : new Animated.ValueXY(0,0)
+       }
+       
    }
 
-   componentDidMount() {
-    Animated.spring(this.mySquare, {
-        toValue : {x : 50, y : 300}
+   runAnimation = () => {
+    Animated.timing(this.state.mySquare, {
+        toValue : {x : 50, y : 300},
+        duration : 2000, // 2초
+        delay : 1500, // 1.5초 후에 동작
+        // easing : Easing.bounce // 마지막 위치에서 통통 튀는 효과
+        easing : Easing.elastic(3) // 더 멀리 갔다가 엔딩포인트에 수렴
     }).start();
    }
 
    render() {
      return (
+         <View>
          <Animated.View
-         style={this.mySquare.getLayout()}
+         style={this.state.mySquare.getLayout()}
             // style={{
             //     left : this.mySquare.x,
             //     top: this.mySquare.y
@@ -28,6 +36,11 @@
             </View>
          </Animated.View>
  
+        <Button
+            title="Animation Start"
+            onPress={this.runAnimation}
+        />
+         </View>
      )
    }
  }
