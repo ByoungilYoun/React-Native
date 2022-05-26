13337cc9752df5296227f9431c892804046f71c4
YounByoungil <jimmy7525@naver.com>
2022-05-26 17:56:40 +0900

    [feat] 다른 Animation 효과 생성하기 - 투명도

--- react_animation/src/Animation01.js
+++ react_animation/src/Animation01.js
@@ -7,18 +7,18 @@
    constructor() {
        super();
        this.state = {
-        mySquare : new Animated.ValueXY(0,0)
+        mySquare : new Animated.Value(1)
        }
        
    }
 
    runAnimation = () => {
     Animated.timing(this.state.mySquare, {
-        toValue : {x : 50, y : 300},
+        toValue : 0,
         duration : 2000, // 2초
         delay : 1500, // 1.5초 후에 동작
         // easing : Easing.bounce // 마지막 위치에서 통통 튀는 효과
-        easing : Easing.elastic(3) // 더 멀리 갔다가 엔딩포인트에 수렴
+        // easing : Easing.elastic(3) // 더 멀리 갔다가 엔딩포인트에 수렴
     }).start();
    }
 
@@ -26,11 +26,10 @@
      return (
          <View>
          <Animated.View
-         style={this.state.mySquare.getLayout()}
-            // style={{
-            //     left : this.mySquare.x,
-            //     top: this.mySquare.y
-            // }}
+        //  style={this.state.mySquare.getLayout()}
+            style={{
+                opacity : this.state.mySquare
+            }}
          >
             <View style={styles.square}>
             </View>
