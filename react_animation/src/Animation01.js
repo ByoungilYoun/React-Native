0a7853811625c9f668b8deb894d5cfd34dffb95f
YounByoungil <jimmy7525@naver.com>
2022-05-26 18:03:05 +0900

    [feat] Interpolation(Animation 효과 조합) 01

--- react_animation/src/Animation01.js
+++ react_animation/src/Animation01.js
@@ -7,14 +7,14 @@
    constructor() {
        super();
        this.state = {
-        mySquare : new Animated.Value(1)
+        mySquare : new Animated.Value(1) // 투명도 1에서 
        }
        
    }
 
    runAnimation = () => {
     Animated.timing(this.state.mySquare, {
-        toValue : 0,
+        toValue : 0, // 투명도 0으로 
         duration : 2000, // 2초
         delay : 1500, // 1.5초 후에 동작
         // easing : Easing.bounce // 마지막 위치에서 통통 튀는 효과
@@ -28,7 +28,11 @@
          <Animated.View
         //  style={this.state.mySquare.getLayout()}
             style={{
-                opacity : this.state.mySquare
+                opacity : this.state.mySquare,
+                top : this.state.mySquare.interpolate({
+                    inputRange :  [0,1], // constructor 내부와 runAnimation 내부에 선언해둔 property 값이 들어감, 여기선 투명도, 작은값부터
+                    outputRange : [700, 0] // 동시에 효과를 내주고싶은 property 값이 들어간다.여기선 y 축 좌표값
+                })
             }}
          >
             <View style={styles.square}>
