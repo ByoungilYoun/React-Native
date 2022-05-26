9f6949cd464bccb90f747904bfba9ddccc2f1f0d
YounByoungil <jimmy7525@naver.com>
2022-05-26 16:25:12 +0900

    [feat] 카메라 & 이미지 갤러리 사용하기

--- react_native_ImagePicker/App.js
+++ react_native_ImagePicker/App.js
@@ -8,13 +8,48 @@
 
 import React, { Component } from 'react';
 import { StyleSheet, View, Text, Image, Button } from 'react-native';
-import ImagePicker from 'react-native-image-picker'
+import ImagePicker from 'react-native-image-picker';
 
 class App extends Component {
+  state = {
+    avatar : ''
+  }
+
+  addImage = () => {
+    // ImagePicker.launchCamera({}, response=>{
+    //   this.setState({
+    //     avatar : response.uri
+    //   })
+    // }) // 카메라 켜기
+
+    ImagePicker.launchImageLibrary({
+
+      title : 'Choose your photo'
+    }, response=>{
+      this.setState({
+        avatar : response.uri
+      })
+    }) // 이미지 라이브러리에서 가져오기 
+
+    // ImagePicker.showImagePicker({}, response=>{
+    //   this.setState({
+    //     avatar : response.uri
+    //   })
+    // }) // 이미지 라이브러리에서 가져오기 
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Hello</Text>
+        <Image
+          source={{uri:this.state.avatar}}
+          style={styles.avatar}
+        />
+
+        <Button
+          title="Add an Image"
+          onPress={()=>this.addImage()}
+        />
       </View>
     )
   }
@@ -26,6 +61,10 @@ const styles = StyleSheet.create({
     justifyContent : 'center',
     alignItems : 'center',
     backgroundColor : '#e4ab26'
+  },
+  avatar : {
+    width : '100%',
+    height : 400
   }
 })
 
