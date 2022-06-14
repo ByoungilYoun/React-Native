94607f7c9f0241cec6f58c5095b004065af4115b
YounByoungil <jimmy7525@naver.com>
2022-06-14 16:50:31 +0900

    [feat] 외부 스타일 이용하기

--- react-native-style/src/App.js
+++ react-native-style/src/App.js
@@ -1,35 +1,15 @@
 import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { View, Text } from 'react-native';
+import { viewStyles, textStyles } from './styles';
 
 
 const App = () => {
     return (
-        <View style={styles.container}>
-            <Text style={styles.text}>Class Styling - Text</Text>
-            <Text style={[styles.text, styles.error]}>Class Styling - Error</Text> 
+        <View style={viewStyles.container}>
+            <Text style={[textStyles.text, {color : 'green'}]}>Class Styling - Text</Text>
+            <Text style={[textStyles.text, textStyles.error]}>Class Styling - Error</Text> 
         </View>
     )
 }
-
-const styles = StyleSheet.create({
-    container : {
-        flex : 1,
-        backgroundColor : '#fff',
-        alignItems : 'center',
-        justifyContent : 'center'
-    },
-    text : {
-        padding : 10,
-        fontSize : 26, 
-        fontWeight : '600',
-        color : 'black'
-    },
-    error : {
-        // padding : 10,
-        // fontSize : 26,
-        fontWeight : '400',
-        color : 'red'
-    }
-})
 
 export default App;
